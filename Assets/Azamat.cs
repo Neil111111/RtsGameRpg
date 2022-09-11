947ef87aa4af4da4b564a4b5fccb8c78f3dede10
LaNdeLSt <landelst36@gmail.com>
2022-09-11 13:16:43 +0500

    Enemy

---
+++ Assets/Azamat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Azamat : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject pcamera;
+
+
+    void OnMouseDown()
+    {
+
+        Debug.Log ("Удар! БУМ!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
